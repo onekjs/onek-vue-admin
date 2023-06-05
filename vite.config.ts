35a3d5d186b98dd4ddaff83bc1861b7fa34b2cda
liuqi <70128222+l1uqi@users.noreply.github.com>
2023-06-05 17:01:30 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import path from "path";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from 'unplugin-vue-components/vite'
+
+import {
+  ArcoResolver
+} from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@views":  path.resolve(__dirname, "src/views"),
+    },
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      /* options */
+      include: [
+        /\.[tj]sx?$/, 
+        /\.vue$/, 
+        /\.vue\?vue/, 
+        /\.md$/,
+      ],
+      imports: [
+        // 插件预设支持导入的api
+        'vue',
+        'vue-router',
+        'pinia'
+        // 自定义导入的api
+      ],
+      dts: './auto-imports.d.ts',
+    }),
+    Components({
+      resolvers:[
+        ArcoResolver()
+      ]
+    })
+  ],
+});
